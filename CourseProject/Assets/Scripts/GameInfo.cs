11f56a8cc26474805b7023d91e1115afeda93b4d
jthra037 <its_thrasher@live.ca>
2018-01-31 23:06:56 -0500

    Add Unity Project Add basic tile object Add tile activation functions

---
+++ CourseProject/Assets/Scripts/GameInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameInfo
+{
+    private static int score = 0;
+    private static bool scanning = false;
+
+    public static int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public static bool Scanning
+    {
+        get { return scanning; }
+        set { scanning = value; }
+    }
+
+
+}
---
+++ CourseProject/Assets/Scripts/GameTile.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Directions
+{
+    north,
+    east,
+    south,
+    west
+}
+
+public enum TileMode
+{
+    minimal,
+    quarter,
+    half,
+    maximum
+}
+
+public class TileInfo
+{
+    public Color color = Color.gray;
+    public int value = 1;
+
+    public TileInfo(TileMode mode = TileMode.minimal)
+    {
+        switch(mode) 
+        {
+            case TileMode.maximum:
+                value = 16;
+                color = Color.yellow;
+                break;
+            case TileMode.half:
+                value = 8;
+                color = new Color(255, 70, 0); //orange
+                break;
+            case TileMode.quarter:
+                value = 4;
+                color = Color.red;
+                break;
+        }
+
+    }
+}
+
+[RequireComponent(typeof(Image), typeof(Button))]
+public class GameTile : MonoBehaviour {
+    public bool isScanned = false;
+    public TileInfo Info = new TileInfo();
+
+    public TileMode Mode
+    {
+        get { return mode; }
+        set
+        {
+            mode = value;
+            Info = new TileInfo(mode);
+        }
+    }
+
+
+    private TileMode mode;
+    private Image image;
+    private Button button;
+    private GameTile[] neighbors = new GameTile[4];
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+    }
+
+    public void TileActivation()
+    {
+        image.color = Info.color;
+        GameInfo.Score += Info.value;
+
+        Mode = TileMode.minimal;
+
+        Debug.Log(GameInfo.Score);
+    }
+}
