699415daa412a9f2d3ad62fc860b57307718eed5
jthra037 <its_thrasher@live.ca>
2018-02-05 18:14:38 -0500

    Set up scene for assignment 2

---
+++ CourseProject/Assets/Scripts/PickController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PickController : MonoBehaviour {
+
+    [SerializeField] private float vertForce = 15;
+    [SerializeField] private float horzVelocity = 5;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Debug.Log(Input.GetAxis("Vertical"));
+
+        rb.velocity = new Vector3(rb.velocity.x, 
+            rb.velocity.y, 
+            horzVelocity * Input.GetAxis("Horizontal"));
+
+        rb.AddForce(transform.up * vertForce * Input.GetAxis("Vertical"));
+	}
+}
---
+++ CourseProject/Assets/Scripts/TorsionFake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class TorsionFake : MonoBehaviour {
+
+    [SerializeField] private float movemementScale = 2;
+    [SerializeField] private Slider sliderRef;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 3;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rb.AddForce(transform.right * movemementScale * sliderRef.value * Time.fixedDeltaTime);
+	}
+}
